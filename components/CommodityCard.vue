9877804cd1199d500c46057d785591924da791dc
Aitm Keldi <xa210296@gmail.com>
2025-07-02 20:17:13 +0500

    feat(commodities): implement commodities page layout

---
+++ components/CommodityCard.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col gap-3">
+    <NuxtImg :src="item.image" />
+    <div class="flex justify-between gap-3">
+      <h2 class="text-3xl">{{ item.title }}</h2>
+      <div class="space-y-1 bg-secondary text-secondary-foreground py-6 px-5.5 rounded-xl xs:space-y-2 xs:max-w-[300px]">
+        <p
+          v-for="(spec, i) in item.specs"
+          :key="i"
+          class="text-xs"
+        >
+          {{ spec }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  item: {
+    title: string
+    image: string
+    specs: string[]
+  }
+}>()
+</script>
--- pages/commodities.vue
+++ pages/commodities.vue
@@ -1,5 +1,217 @@
+<script setup lang="ts">
+const commodities = [
+    {
+        title: 'Corn',
+        image: '/commodities/1.png',
+        specs: [
+            'Moisture: max. 14.5 %',
+            'Foreign matter: max. 2 %',
+            'Damaged kernels: max. 5 % (including max 2% heat damaged)',
+            'Broken kernels: max. 5 %',
+        ],
+    },
+    {
+        title: 'Feed Wheat',
+        image: '/commodities/2.png',
+        specs: [
+            'Test weight: Min 72 Kg/hl',
+            'Moisture: Max 14%',
+            'Heated Grain: Max 0.5%',
+            'Sprouted Grain: Max 1%',
+            'Broken Grain: Max 15%',
+            'Foreign Matter: Max 2%',
+        ],
+    },
+    {
+        title: 'Milling Wheat',
+        image: '/commodities/3.png',
+        specs: [
+            'Test Weight 77 kg/hl min',
+            'Moisture: 14% max',
+            'Protein: 11.5% min',
+            'Wet Gluten: 23% min (ISO 21415-1)',
+            'Falling Number: 230 sec min (Hagberg)',
+            'W: 160 min (ISO)',
+            'Foreign Matter: 2% max',
+            'Bug Damaged: 2% max',
+        ],
+    },
+    {
+        title: 'Barley',
+        image: '/commodities/4.png',
+        specs: [
+            'Test Weight: 62 kg/hl min',
+            'Moisture: 14% max',
+            'Foreign Matter: 2% max',
+        ],
+    },
+    {
+        title: 'Soy-beans',
+        image: '/commodities/5.png',
+        specs: [
+            'Moisture Max 13.0%',
+            'Oil content Min 18.5%',
+            'Protein Min 33%',
+            'Foreign matters Max 2%',
+            'Damaged kernels Max 5%',
+            'Splits Max 20%',
+        ],
+    },
+    {
+        title: 'Green / Yellow peas',
+        image: '/commodities/6.png',
+        specs: [
+            'Moisture 14% max',
+            'Halves and broken: 8% max',
+            'Bulk admixture: 2% max',
+            'Other color: 5% max',
+            'Damaged: 3% max (including damaged by insect)',
+        ],
+    }, 
+    {
+        title: 'Sun-flower Meal',
+        image: '/commodities/7.png',
+        specs: [
+            'Protein: 35% min',
+            'Fibre: 20.0 % max',
+            'Moisture: 12% max',
+            'Oil: max 2%',
+        ],
+    }, 
+    {
+        title: 'Wheat Flour',
+        image: '/commodities/8.png',
+        specs: [
+            'Moisture: max 14.6%',
+            'Gluten on wet basis: Min 24%',
+            'Whiteness: Min 48%',
+            'Falling number: Min 280%',
+            'Protein: Min 10.5%',
+            'Ash content: 0.65%',
+        ],
+    }, 
+    {
+        title: 'Flax Seeds',
+        image: '/commodities/9.png',
+        specs: [
+            'Moisture: max 13%',
+            'Foreign matters: Max 3%',
+            'Foreign Oil admixture: max 6%',
+        ],
+    }, 
+    {
+        title: 'Lentils',
+        image: '/commodities/10.png',
+        specs: [
+            'Moisture: max 15%',
+            'Protein on dry: 21% min',
+            'Foreign matters: Max 0.5 %',
+            'Grain admixture: max 3%',
+        ],
+    }, 
+    {
+        title: 'Oats & Graid',
+        image: '/commodities/11.png',
+        specs: [
+            'Moisture: max 13.5 %',
+            'Test weight: Min 520',
+            'Foreign matter: max 2 %',
+        ],
+    }, 
+    {
+        title: 'Feed Oats',
+        image: '/commodities/12.png',
+        specs: [
+            'Moisture: max 13.5 %',
+            'Test weight: Min 460',
+            'Foreign matter: max 3%',
+        ],
+    }, 
+    {
+        title: 'Milletе',
+        image: '/commodities/13.png',
+        specs: [
+            'Moisture: max 13.5 %',
+            'Foreign matter: max 2 %',
+        ],
+    }, 
+    {
+        title: 'Sorg-hum',
+        image: '/commodities/14.png',
+        specs: [
+            'Moisture: max 14 %',
+            'Foreign matter: max 5%',
+        ],
+    }, 
+    {
+        title: 'Soybean Meal',
+        image: '/commodities/15.png',
+        specs: [
+            'Protein: 46 % min',
+            'Fibre: 6 % max',
+            'Crude fat: max 2%',
+            'Urease between 0.06-0.15',
+            'Urea free',
+        ],
+    }, 
+    {
+        title: 'Sunflo-wer Oil',
+        image: '/commodities/16.png',
+        specs: [
+            'FFA: 2% max 3%',
+            'Moisture and Impurities: max 0.5 %',
+            'Flash point: no less than 121 degrees Celsius',
+            'Urease between 0.06-0.15',
+            'Hydrocarbons Content: max 50 mg/kg',
+        ],
+    }, 
+    {
+        title: 'Sunflo-wer Cake',
+        image: '/commodities/17.png',
+        specs: [
+            'Protein: 32% min',
+            'Fibre: 21.0 % max',
+            'Moisture: 9% max',
+            'Oil: max 7%',
+        ],
+    }, 
+    {
+        title: 'Soybean Cake',
+        image: '/commodities/18.png',
+        specs: [
+            'Protein: 44% mint',
+            'Fibre: 5.5 % max',
+            'Crude fat: max 8%',
+            'Moisture: max 11 %',
+        ],
+    }, 
+    {
+        title: 'Crude Soybean Oil',
+        image: '/commodities/19.png',
+        specs: [
+            'Color (Lovibond cell 1 inch) 5 r and 50 y',
+            'Free fatty acids (% as oleic) up to 1,25',
+            'Moisture and volatile matter (%) up to 0,20%',
+            'Impurities: max 0.10%',
+            'Sediment: Max 0.10%',
+            'Total phosphorus (%) up to 0,020%',
+            'Unsaponifiable matter up to 1,5%',
+        ],
+    },
+    // добавь другие товары по аналогии
+]
+</script>
+
 <template>
-    <div>
-        commodities
+    <div class="pt-8 pb-15">
+        <div class="container">
+            <div class="grid xs:grid-cols-(--my-grid-cols) gap-5">
+                <CommodityCard
+                    v-for="(item, index) in commodities"
+                    :key="index"
+                    :item="item"
+                />
+            </div>
+        </div>
     </div>
 </template>
