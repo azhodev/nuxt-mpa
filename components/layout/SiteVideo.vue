552e9b4425e497a4e921113cbedaf858d754325f
Avaz Hojakeldiyev <azho.dev@gmail.com>
2025-07-04 12:29:40 +0500

    perf(hero): update WebP poster for video background

--- components/layout/SiteVideo.vue
+++ components/layout/SiteVideo.vue
@@ -1,7 +1,7 @@
 <template>
     <video
         class="fixed top-0 w-full h-full object-cover -z-10"
-        poster="/bg-poster.webp"
+        poster="/bg-video-poster.webp"
         muted
         playsinline
         autoplay
