c2bc67e554b8ae219ce4c1cb646ba5c32b66efb7
Avaz Hojakeldiyev <azho.dev@gmail.com>
2025-08-15 22:53:20 +0500

    refactor: rename-layout-components-to-app

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,18 @@
 <script setup>
-const LayoutFooter = defineAsyncComponent(() => import('~/components/LayoutFooter.vue'))
-const LayoutHeader = defineAsyncComponent(() => import('~/components/LayoutHeader.vue'))
+const AppFooter = defineAsyncComponent(() => import('~/components/AppFooter.vue'))
+const AppHeader = defineAsyncComponent(() => import('~/components/AppHeader.vue'))
 const { isHomePage } = useRouteInfo()
 </script>
 
 <template>
   <div class="relative flex flex-col min-h-screen">
-    <LayoutHeader />
+    <AppHeader />
 
     <HeroSection :is-home-page="isHomePage" />
     <main class="bg-background min-h-screen flex flex-col align-middle flex-1">
       <slot />
     </main>
     
-    <LayoutFooter />
+    <AppFooter />
   </div>
 </template>
